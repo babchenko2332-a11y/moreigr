36f7052f5142a29e525dc4894c144131bf9a2eb5
babchenko2332-a11y <babchenko2332@gmail.com>
2026-01-30 00:24:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+const tg = window.Telegram.WebApp;
+tg.ready();
+tg.expand();
+
+const products = [
+  {
+    name: "Игра 1",
+    price: "199 ₽",
+    image: "https://via.placeholder.com/300"
+  },
+  {
+    name: "Игра 2",
+    price: "299 ₽",
+    image: "https://via.placeholder.com/300"
+  }
+];
+
+const container = document.getElementById("products");
+
+products.forEach(p => {
+  container.innerHTML += `
+    <div class="card">
+      <img src="${p.image}">
+      <h3>${p.name}</h3>
+      <p>${p.price}</p>
+    </div>
+  `;
+});
+
 
